6fd9c3b4da13114bc34238f1626fcba07c3ea2ee
tabish1235 <tabish554@gmail.com>
2021-09-20 23:46:57 +0500

    weather app complete now time for styling

--- src/App.js
+++ src/App.js
@@ -4,40 +4,88 @@ import './App.css';
 import { Input, Button } from './components';
 import { useState, useEffect } from "react"
 function App() {
-  const [data, setData] = useState("")
+  // const [data, setData] = useState("")
   const [click, setClick] = useState(true);
   const [city, setCity] = useState("");
+  const [weather, setWeather] = useState("");
 
+  let lat;
+  let lon;
 
-useEffect(() => {
-if(city){
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0c2cae07ef5408abb344eee6a90c0874`)
-  .then(response => response.json())
-  .then(json => console.log(json)
-  ).catch((err) => {
-    console.log(err)
-  })
-}else{
-  
-}
-},[city])
+  useEffect(() => {
+    if (city) {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0c2cae07ef5408abb344eee6a90c0874`)
+        .then(response => response.json())
+        .then(json => setWeather(json)
+        ).catch((err) => {
+          console.log(err)
+        })
+    } else {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)
+      } else {
+        alert("Geolocation is not supported by this browser.");
+      }
+      function showPosition(position) {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0c2cae07ef5408abb344eee6a90c0874`)
+          .then(response => response.json())
+          .then(json => setWeather(json)
+          ).catch((err) => {
+            console.log(`Something went wrong!\n${err}`)
 
-let searchcity=()=>{
-  console.log(document.getElementById("search").value)    
-  setCity(document.getElementById("search").value)
-  console.log(city)    
+          })
+      }
+    }
+  }, [city, lat, lon])
 
+  let searchcity = () => {
+    console.log(document.getElementById("search").value)
+    setCity(document.getElementById("search").value)
+    // console.log(weather)
+    setClick(!click)
 
-      setClick(!click)
-}
-  return (
-    <div>
-      <div className="searchBar">
-      <Input id="search" placeholder="Enter City Name" type="text" myClass="myInput"/>
-      <Button onClick={()=>searchcity()} myClass="myBtn">Click Me</Button>
+  }
+  if (weather) {
+    return (
+      <div>
+        <div className="searchBar">
+          <Input id="search" placeholder="Enter City Name" type="text" myClass="myInput" />
+          <Button onClick={() => searchcity()} myClass="myBtn">Click Me</Button>
+        </div>
+        <div>
+          <h1>{weather.name}</h1>
+          <h3 >{weather.weather[0].description}</h3>
+          <h1 >{weather.main.temp}'C</h1>
+          <h4>Latitude: {weather.coord.lat}</h4>
+          <h4>Longitude: {weather.coord.lon}</h4>
+        </div>
+        <div>
+          <ul>
+            <li>Country : {weather.sys.country}</li>
+            <li>Feels Like : {weather.main.feels_like}'C</li>
+            <li>Humidity : {weather.main.humidity}g/kg</li>
+            <li>Pressure : {weather.main.pressure} Pa</li>
+            <li>Max Temperature : {weather.main.temp_max}'C</li>
+            <li>Min Temperature : {weather.main.temp_min}'C</li>
+            <li>Wind Direction : {weather.wind.deg} degree</li>
+            <li>Wind Speed : {weather.wind.speed}m/s</li>
+          </ul>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div>
+        <div className="searchBar">
+          <Input id="search" placeholder="Enter City Name" type="text" myClass="myInput" />
+          <Button onClick={() => searchcity()} myClass="myBtn">Click Me</Button>
+        </div>
+      </div>
+    )
+
+  }
 }
 
 export default App;
